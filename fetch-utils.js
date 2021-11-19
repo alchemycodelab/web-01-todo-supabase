0bfb317fba48694eee4506e54f3c67f19190b3f8
Danny Cairns <dpcairns@gmail.com>
2021-11-18 17:13:06 -0800

    initial commit of supabase

---
+++ fetch-utils.js
@@ -0,0 +1,43 @@
+const SUPABASE_URL = '';
+const SUPABASE_KEY = '';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getUser() {
+    return client.auth.session();
+}
+
+
+export async function checkAuth() {
+    const user = await getUser();
+
+    if (!user) location.replace('../'); 
+}
+
+export async function redirectIfLoggedIn() {
+    if (await getUser()) {
+        location.replace('./other-page');
+    }
+}
+
+export async function signupUser(email, password){
+    const response = await client.auth.signUp({ email, password });
+    
+    return checkError(response);
+}
+
+export async function signInUser(email, password){
+    const response = await client.auth.signIn({ email, password });
+
+    return checkError(response);
+}
+
+export async function logout() {
+    await client.auth.signOut();
+
+    return window.location.href = '/';
+}
+
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
